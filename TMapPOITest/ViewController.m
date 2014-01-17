90ac71cc46be2313d0717185bd37230a63b749a7
ChungYeul In <sldk86@naver.com>
2014-01-17 13:58:38 +0900

    p231 6.4.2. 실습입니다.
    p231 6.4.2. 실습입니다.

--- TMapPOITest/ViewController.m
+++ TMapPOITest/ViewController.m
@@ -7,17 +7,48 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "TMapView.h"
+#define TOOLBAR_HEIGHT 64
+@interface ViewController ()<UISearchBarDelegate, TMapViewDelegate>
+@property (strong, nonatomic) TMapView *mapView;
 @end
 
 @implementation ViewController
-
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+    [self.mapView clearCustomObjects];
+    
+    NSString *keyword = searchBar.text;
+    TMapPathData *path = [[TMapPathData alloc] init];
+    NSArray *result = [path requestFindTitlePOI:keyword];
+    NSLog(@"Number of POI : %d", result.count);
+    
+    int i = 0;
+    for (TMapPOIItem *item in result) {
+        NSLog(@"Name : %@ - Point : %@", [item getPOIName], [item getPOIPoint]);
+        
+        NSString *markerID = [NSString stringWithFormat:@"marker_%d", i++];
+        TMapMarkerItem *marker = [[TMapMarkerItem alloc] init];
+        [marker setTMapPoint:[item getPOIPoint]];
+        [marker setIcon:[UIImage imageNamed:@"red_pin.png"]];
+        
+        [marker setCanShowCallout:YES];
+        [marker setCalloutTitle:[item getPOIName]];
+        [marker setCalloutSubtitle:[item getPOIAddress]];
+        
+        [self.mapView addCustomObject:marker ID:markerID];
+    }
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //
+    CGRect rect = CGRectMake(0, TOOLBAR_HEIGHT, self.view.frame.size.width, self.view.frame.size.height - TOOLBAR_HEIGHT);
+    self.mapView = [[TMapView alloc] initWithFrame:rect];
+    [self.mapView setSKPMapApiKey:@"82add192-7843-36bc-b1fd-175e2c70faad"];
+    self.mapView.delegate = self;
+    
+    [self.view addSubview:self.mapView];
 }
 
 - (void)didReceiveMemoryWarning
